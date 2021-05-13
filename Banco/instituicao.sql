ec386211ad1249555a40c88d97832d47d5882256
viniquadros1997 <viniquadros1997@gmail.com>
2021-05-12 23:05:30 -0300

    Atualizando o banco (ajustar ainda)

--- Banco/instituicao.sql
+++ Banco/instituicao.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28-Abr-2021 às 06:04
+-- Tempo de geração: 13-Maio-2021 às 04:03
 -- Versão do servidor: 10.4.18-MariaDB
 -- versão do PHP: 8.0.3
 
@@ -45,69 +45,9 @@ CREATE TABLE `instituicao` (
 
 INSERT INTO `instituicao` (`id_ong`, `instituicao_ong`, `email_ong`, `endereco_ong`, `cnpj_ong`, `senha_ong`, `cep_ong`, `telefone_ong`, `descricao_ong`) VALUES
 (1, 'Flordelotus@gmail.com', 'Flor de Lotus', '649.545.656/5655-55', '123', 'f69e748f79dd0a3848f8b78a15f46892', '892', '47', 'Flor de Lotus é uma instituição que ajuda varias famílias carentes com cestas básicas em Joinville.'),
-(2, 'Flor de Lotus', 'Flordelotus@gmail.com', 'Rua casa grande,210', '649545', '958df7f237e24c5627644ade47fb8f16', '892', '47', 'Intituição top');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
-
-CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
-  `nome_usuario` varchar(20) NOT NULL,
-  `sobrenome_usuario` varchar(20) NOT NULL,
-  `cpf_usuario` int(11) NOT NULL,
-  `email_usuario` varchar(100) NOT NULL,
-  `senha_usuario` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `usuario`
---
-
-INSERT INTO `usuario` (`id_usuario`, `nome_usuario`, `sobrenome_usuario`, `cpf_usuario`, `email_usuario`, `senha_usuario`) VALUES
-(1, '$nome', '$sobrenome', 0, '$email', '$senha'),
-(2, 'nome', 'Quadros', 2147483647, 'vini@uni.com', '0123456'),
-(3, '', '', 2147483647, 'vini@uniesu.com.br', '124bd1296bec0d9d'),
-(4, '', '', 2147483647, 'viniquadros1997@gmai', '124bd1296bec0d9d'),
-(5, '', '', 2147483647, 'viniquadros1997@gmai', 'eb62f6b9306db575'),
-(6, 'Vini', 'Quadros', 109276, 'viniquadros1997@gmai', '781e5e245d69b566'),
-(7, 'André Quadros', 'Quadros', 646545, 'viniquadros@gmail.com', '4100c4d44da9177247e44a5fc1546778'),
-(8, 'everton', 'uni', 109222, 'everton@gmail.com', 'eb62f6b9306db575c2d596b1279627a4'),
-(9, 'Tiago', 'Schneider', 654165, 'tiagoschneider1@gmail.com', 'eb62f6b9306db575c2d596b1279627a4');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `instituicao`
---
-ALTER TABLE `instituicao`
-  ADD PRIMARY KEY (`id_ong`);
-
---
--- Índices para tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `instituicao`
---
-ALTER TABLE `instituicao`
-  MODIFY `id_ong` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de tabela `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+(2, 'Flor de Lotus', 'Flordelotus@gmail.com', 'Rua casa grande,210', '649545', '958df7f237e24c5627644ade47fb8f16', '892', '47', 'Intituição top'),
+(0, 'akndbsabd', 'ivan@gmail.com', 'asjhdsadnsadhnsadosajdlsajlk', '231.321.321/321', 'eb62f6b9306db575c2d596b1279627a4', '64.553-1', '12 31321-23', 'asdsadasd'),
+(0, 'sfasfsafas', 'vini@gmail.com', 'sasdaoskndkasndpks', '465.465.456/456', 'eb62f6b9306db575c2d596b1279627a4', '68.746-4', '65 46545-64', 'dasdasdasdsadsa');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Banco/usuario.sql
+++ Banco/usuario.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27-Abr-2021 às 03:34
--- Versão do servidor: 10.4.13-MariaDB
--- versão do PHP: 7.4.8
+-- Tempo de geração: 13-Maio-2021 às 04:03
+-- Versão do servidor: 10.4.18-MariaDB
+-- versão do PHP: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,25 +30,31 @@ SET time_zone = "+00:00";
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
   `nome_usuario` varchar(20) NOT NULL,
-  `sobrenome_usuario` varchar(20) NOT NULL,
   `cpf_usuario` int(11) NOT NULL,
   `email_usuario` varchar(100) NOT NULL,
-  `senha_usuario` varchar(32) NOT NULL
+  `senha_usuario` varchar(32) NOT NULL,
+  `telefone_usuario` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `nome_usuario`, `sobrenome_usuario`, `cpf_usuario`, `email_usuario`, `senha_usuario`) VALUES
-(1, '$nome', '$sobrenome', 0, '$email', '$senha'),
-(2, 'nome', 'Quadros', 2147483647, 'vini@uni.com', '0123456'),
-(3, '', '', 2147483647, 'vini@uniesu.com.br', '124bd1296bec0d9d'),
-(4, '', '', 2147483647, 'viniquadros1997@gmai', '124bd1296bec0d9d'),
-(5, '', '', 2147483647, 'viniquadros1997@gmai', 'eb62f6b9306db575'),
-(6, 'Vini', 'Quadros', 109276, 'viniquadros1997@gmai', '781e5e245d69b566'),
-(7, 'André Quadros', 'Quadros', 646545, 'viniquadros@gmail.com', '4100c4d44da9177247e44a5fc1546778'),
-(8, 'everton', 'uni', 109222, 'everton@gmail.com', 'eb62f6b9306db575c2d596b1279627a4');
+INSERT INTO `usuario` (`id_usuario`, `nome_usuario`, `cpf_usuario`, `email_usuario`, `senha_usuario`, `telefone_usuario`) VALUES
+(1, '$nome', 0, '$email', '$senha', 0),
+(2, 'nome', 2147483647, 'vini@uni.com', '0123456', 0),
+(3, '', 2147483647, 'vini@uniesu.com.br', '124bd1296bec0d9d', 0),
+(4, '', 2147483647, 'viniquadros1997@gmai', '124bd1296bec0d9d', 0),
+(5, '', 2147483647, 'viniquadros1997@gmai', 'eb62f6b9306db575', 0),
+(6, 'Vini', 109276, 'viniquadros1997@gmai', '781e5e245d69b566', 0),
+(7, 'André Quadros', 646545, 'viniquadros@gmail.com', '4100c4d44da9177247e44a5fc1546778', 0),
+(8, 'everton', 109222, 'everton@gmail.com', 'eb62f6b9306db575c2d596b1279627a4', 0),
+(9, 'vinicius', 222222, 'vini@gmail.com', 'eb62f6b9306db575c2d596b1279627a4', 0),
+(10, 'asdjhsakjd', 654654, 'viniq@gmail.com', 'd2490f048dc3b77a457e3e450ab4eb38', 45),
+(11, 'dsfgsdfsdf', 354354, '454654654@gmail.com', 'eb62f6b9306db575c2d596b1279627a4', 65),
+(12, 'skjdbahdsa', 564654, 'vvvv@gmail.com', 'eb62f6b9306db575c2d596b1279627a4', 56),
+(13, 'marlon', 453545, 'marlon@gmail.com', 'eb62f6b9306db575c2d596b1279627a4', 35),
+(14, 'andriew', 654654, 'a@gmail.com', 'eb62f6b9306db575c2d596b1279627a4', 64);
 
 --
 -- Índices para tabelas despejadas
@@ -68,7 +74,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
